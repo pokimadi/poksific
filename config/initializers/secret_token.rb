bba6b3dfaef11dabb47e4532d79674cf931e22dd
pokimadi <pokimadim@hotmail.com>
2012-09-30 00:08:19 -0400

    Poksific start-up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poksific::Application.config.secret_token = '1dd847b5ec20e7c3a2d3ec02ea06c6e76ad759cc67e9e8fef407cc49d7e6531150c512b3067db240cc59f2eed13ab2409406512f5117b422ec93039af03e13e2'
