ba9cd72c540f07493772f15fc09a0372d2b9b442
pokimadi <pokimadim@hotmail.com>
2012-10-02 21:23:21 -0400

    Sessions, Admin deletion privileges and menu bar are now functional.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: [:index, :destroy]
+
+
   def show
     @user = User.find(params[:id])
   end
@@ -6,6 +11,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
 
   def create
     @user = User.new(params[:user])
@@ -18,5 +28,44 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+
+  def edit
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    #TODO: Remove email update.
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def signed_in_user
+      store_location
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,11 @@ module SessionsHelper
     @current_user = user
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+
+  
   def signed_in?
     !current_user.nil?
   end
@@ -22,6 +27,16 @@ module SessionsHelper
     current_user = nil
     cookies.delete(:remember_token)
   end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
 
 
   
